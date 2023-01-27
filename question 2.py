4b803509fed1aaf709138028e5d8c56a70524d8b
devanshvashisht <121232078+devanshvashisht@users.noreply.github.com>
2023-01-27 22:16:20 +0530

    Add files via upload

---
+++ question 2.py
@@ -0,0 +1,25 @@
+a=int(input("Enter the year: "))
+
+if a%4==0:
+    if a%100==0 and a%400!=0:
+        print("It is not a leap year")
+    
+    else:
+        print("It is a leap year")
+
+else:
+    print("It is not a leap year")        
+    
+
+
+    
+
+
+
+
+
+
+    
+
+
+
---
+++ question 3.py
@@ -0,0 +1,18 @@
+import random 
+
+i=1
+while (i<=10):
+      r1=random.randint(0,9)
+      r2=random.randint(0,9)
+       
+      print(r1,"*",r2,"=")
+      Answer=int(input("Enter the answer \n"))
+
+      if Answer==r1*r2:
+            print("Right!")
+
+      else:
+            print("Wrong")
+      i=i+1
+print("Done")
+                                                                            
